92a081d31503b688fa0df89fd1614bd654ed4fc7
ganthegun <ganjunwei020522@gmail.com>
2024-01-19 12:25:35 +0800

    added stack

--- DataSA_project.c
+++ DataSA_project.c
@@ -85,27 +85,27 @@ int main() {
     int option, chooseBook;
     int enqueueTimes = 0;
     struct Book libraryBook[10] = {
-        {"Time Travel", "Borneo News", "978-000-000-000-0", "History", "English", 2010, 2, 310, {"Jojo", "Malaysian", "PhD", "History Award", "I believe in history"}}, 
-        {"Learn Java", "Starch Press", "978-111-111-111-1", "Programming", "Hindi", 2016, 5, 158, {"Koko", "Indian", "Master", "Programming Award", "I believe in programming"}}, 
-        {"Sapiens Brief", "Harper Col", "978-222-222-222-2", "History", "German", 1951, 1, 160, {"Lolo", "German", "Master", "History Award", "I love history"}},
-        {"Silent Patient", "Cell Store", "978-333-333-333-3", "Thriller", "English", 2023, 3, 63, {"Momo", "American", "PhD", "Thriller Award", "I believe in thriller"}}, 
-        {"Edu Learn", "Random Home", "978-444-444-444-4", "Memoir", "Japanese", 2001, 8, 189, {"Nono", "Japanese", "PhD", "Memoir Award", "I believe in memoir"}}, 
-        {"Teh Tarik", "Cetakan UMPSA", "978-555-555-555-5", "Foods", "Malay", 2001, 3, 92, {"Popo", "Malaysian", "PhD", "Foods Award", "Food is everything"}}, 
-        {"The Alchemist", "Nature Grp", "978-666-666-666-6", "Fiction", "Spanish", 2003, 6, 40, {"Roro", "Filipino", "Master", "Fiction Award", "I believe in fiction"}}, 
-        {"Science Advances", "GIT Press", "978-777-777-777-7", "Science", "English", 1999, 9, 307, {"Soso", "Bengali", "Master", "Science Award", "I believe in science"}}, 
-        {"Global Economic", "GIT Press", "978-888-888-888-8", "Economics", "Thai", 2024, 12, 524, {"Vovo", "Thai", "PhD", "Economics Award", "I believe in economics"}}, 
-        {"AI Research", "Hangul News", "978-999-999-999-9", "Technology", "Korean", 2005, 8, 256, {"Yoyo", "Korean", "Master", "Technology Award", "I believe in technology"}}
+        {"Time Travel", "Borneo News", "978-000-000-000-0", "History", "English", 2010, 2, 310, {"Jojo", "Malaysian", "PhD", "History Award", "I believe in history"}, NULL, NULL},
+        {"Learn Java", "Starch Press", "978-111-111-111-1", "Programming", "Hindi", 2016, 5, 158, {"Koko", "Indian", "Master", "Programming Award", "I believe in programming"}, NULL, NULL},
+        {"Sapiens Brief", "Harper Col", "978-222-222-222-2", "History", "German", 1951, 1, 160, {"Lolo", "German", "Master", "History Award", "I love history"}, NULL, NULL},
+        {"Silent Patient", "Cell Store", "978-333-333-333-3", "Thriller", "English", 2023, 3, 63, {"Momo", "American", "PhD", "Thriller Award", "I believe in thriller"}, NULL, NULL},
+        {"Edu Learn", "Random Home", "978-444-444-444-4", "Memoir", "Japanese", 2001, 8, 189, {"Nono", "Japanese", "PhD", "Memoir Award", "I believe in memoir"}, NULL, NULL},
+        {"Teh Tarik", "Cetakan UMPSA", "978-555-555-555-5", "Foods", "Malay", 2001, 3, 92, {"Popo", "Malaysian", "PhD", "Foods Award", "Food is everything"}, NULL, NULL},
+        {"The Alchemist", "Nature Grp", "978-666-666-666-6", "Fiction", "Spanish", 2003, 6, 40, {"Roro", "Filipino", "Master", "Fiction Award", "I believe in fiction"}, NULL, NULL},
+        {"Science Advances", "GIT Press", "978-777-777-777-7", "Science", "English", 1999, 9, 307, {"Soso", "Bengali", "Master", "Science Award", "I believe in science"}, NULL, NULL},
+        {"Global Economic", "GIT Press", "978-888-888-888-8", "Economics", "Thai", 2024, 12, 524, {"Vovo", "Thai", "PhD", "Economics Award", "I believe in economics"}, NULL, NULL},
+        {"AI Research", "Hangul News", "978-999-999-999-9", "Technology", "Korean", 2005, 8, 256, {"Yoyo", "Korean", "Master", "Technology Award", "I believe in technology"}, NULL, NULL}
     };
     displayLibraryBook(libraryBook);
     searchingSorting(libraryBook); //Searching, sorting
     struct Book* headBook = NULL;
-    struct BookHistory *front= NULL, *rear= NULL; 
+    struct BookHistory *front= NULL, *rear= NULL;
     struct BookHistory *newptrHistory, *temp;
     do {
         printf("\n\n**********************MENU*********************");
         printf("\n1) Add a book to borrow"); //Insert into linked list, Enqueue, Dequeue
         printf("\n2) Start reading"); //Update linked list, Stack
-        printf("\n3) View book history"); 
+        printf("\n3) View book history");
         printf("\n4) Return book"); //Delete from linked list
         printf("\nSelect an option: ");
         scanf("%d", &option);
@@ -129,7 +129,7 @@ int main() {
 void displayLibraryBook(struct Book libraryBook[]) {
     printf("\n**********List of book in our digital library************");
     for(int i = 0; i < 10; i++) {
-        /*printf("\n%d) %s,\t %d,\t version %d,\t %d pages,\tpublisher %s, ISBN %d, genre %s, language %s, author %s, %s, %s, %s, '%s'", i + 1, libraryBook[i].title, libraryBook[i].publicationYear, libraryBook[i].version, libraryBook[i].numPages, libraryBook[i].publisher, libraryBook[i].ISBN, libraryBook[i].genre, libraryBook[i].language, libraryBook[i].author.name, libraryBook[i].author.nationality, libraryBook[i].author.education, libraryBook[i].author.award, libraryBook[i].author.quote);*/
+        printf("\n%d) %s,\t %d,\t version %d,\t %d pages,\tpublisher %s, ISBN %s, genre %s, language %s, author %s, %s, %s, %s, '%s'", i + 1, libraryBook[i].title, libraryBook[i].publicationYear, libraryBook[i].version, libraryBook[i].numPages, libraryBook[i].publisher, libraryBook[i].ISBN, libraryBook[i].genre, libraryBook[i].language, libraryBook[i].author.name, libraryBook[i].author.nationality, libraryBook[i].author.education, libraryBook[i].author.award, libraryBook[i].author.quote);
     }
 }
 
@@ -137,7 +137,7 @@ void searchingSorting(struct Book libraryBook[]) {
     int operation, sortBy, continueOperation, searchingValue;
 
     do {
-        printf("\nSearching or sorting on the list[1 for sort / 2 for search / 3 for NO]: ");
+        printf("\nSearching or sorting on the list[1 for sort / 2 for search / 3 for no]: ");
         scanf("%d", &operation);
         if(operation == 1) {
             printf("Sort by[1 for version / 2 for number of pages]: ");
@@ -148,16 +148,13 @@ void searchingSorting(struct Book libraryBook[]) {
                 sortByNumPages(libraryBook);
             }
             displayLibraryBook(libraryBook);
-            printf("\nContinue sorting or searching on the list?[1 for yes / 2 for no]: ");
-            scanf("%d", &continueOperation);
         } else if(operation == 2) {
             printf("Enter searching value for publication year: ");
             scanf("%d", &searchingValue);
             searching(libraryBook, searchingValue);
-            printf("\nContinue sorting and searching on the list?[1 for yes / 2 for no]: ");
-            scanf("%d", &continueOperation);
-            displayLibraryBook(libraryBook);
         }
+        printf("\nContinue sorting and searching on the list?[1 for yes / 2 for no]: ");
+        scanf("%d", &continueOperation);
     } while((operation == 1 || operation == 2) && continueOperation == 1);
 
 }
@@ -214,7 +211,9 @@ void addBook(struct Book** headBook, struct Book libraryBook[], struct BookHisto
     //Don't borrow the same book
     printf("\nChoose a book to borrow by entering the book number: ");
     scanf("%d", &chooseBook);
+    //index for book chose
     chooseBook = chooseBook - 1;
+    //assign the chosen book into a book node
     struct Book* newBookPtr = (struct Book*) malloc(sizeof(struct Book));
     strcpy(newBookPtr -> title, libraryBook[chooseBook].title);
     strcpy(newBookPtr -> publisher, libraryBook[chooseBook].publisher);
@@ -229,19 +228,24 @@ void addBook(struct Book** headBook, struct Book libraryBook[], struct BookHisto
     strcpy(newBookPtr -> author.education, libraryBook[chooseBook].author.education);
     strcpy(newBookPtr -> author.award, libraryBook[chooseBook].author.award);
     strcpy(newBookPtr -> author.quote, libraryBook[chooseBook].author.quote);
+    //create a node for reading progress
     struct ReadingProgress* newProgressPtr = (struct ReadingProgress*) malloc(sizeof(struct ReadingProgress));
     newProgressPtr -> page = 1;
     newProgressPtr -> nextPtr = NULL;
+    //assign the reading progress node to the newly created book node using queue (enqueue)
     newBookPtr -> readingProgress = newProgressPtr;
     newBookPtr -> nextPtr = NULL;
+    //add the newly created book node into the linked list in main function (by referencing to headbook pointer)
     if((*headBook) == NULL) {
         (*headBook) = newBookPtr;
     } else {
+        //tenporary pointer for traversing
         struct Book* tempBookPtr = (struct Book*) malloc(sizeof(struct Book));
         tempBookPtr = (*headBook);
         while(tempBookPtr -> nextPtr != NULL) {
             tempBookPtr = tempBookPtr -> nextPtr;
         }
+        //enqueue the node into the stack linked list
         tempBookPtr -> nextPtr = newBookPtr;
     }
 
@@ -256,11 +260,12 @@ void addBook(struct Book** headBook, struct Book libraryBook[], struct BookHisto
 		(*rear)->nextPtr = *newptrHistory;
 		*rear = *newptrHistory;
 	}
+    //Dequeue the book history because can borrow maximum three books
 	if(*enqueueTimes > 3) {
 		*temp = *front;
 		*front = (*front)->nextPtr;
 		free(*temp);
-	} //Dequeue the book history
+	}
     printf("Insert Into Book Linked List Successfully\n");
 }
 
@@ -273,9 +278,6 @@ void currentlyReadingBook(struct Book* headBook) {
         tempBookPtr = headBook;
         printf("\nCurrently reading books: ");
         while(tempBookPtr != NULL) {
-            while(tempBookPtr -> readingProgress -> nextPtr != NULL) {
-                tempBookPtr -> readingProgress = tempBookPtr -> readingProgress -> nextPtr;
-            }
             printf("\n%d) %s\t----- Page %d", i + 1, tempBookPtr -> title, tempBookPtr -> readingProgress -> page);
             tempBookPtr = tempBookPtr->nextPtr;
             i++;
@@ -286,8 +288,7 @@ void currentlyReadingBook(struct Book* headBook) {
 void readBook(struct Book* headBook) {
     int chooseBook, page;
 
-    struct Book* tempBookPtr = (struct Book*) malloc(sizeof(struct Book));
-    tempBookPtr = headBook;
+    struct Book* tempBookPtr = headBook;
     if(tempBookPtr != NULL) {
         printf("\nChoose a book to start reading by entering the book number: ");
         scanf("%d", &chooseBook);
@@ -300,22 +301,27 @@ void readBook(struct Book* headBook) {
         if(tempBookPtr == NULL) {
             printf("\nNo such book in your currently reading book. Please enter a valid book number!");
         } else {
-            while(tempBookPtr -> readingProgress -> nextPtr != NULL) {
-                tempBookPtr -> readingProgress = tempBookPtr -> readingProgress -> nextPtr;
-            }
             printf("\n%s\t----- Page %d", tempBookPtr -> title, tempBookPtr -> readingProgress -> page);
             printf("\nUser is reading the book choosen......");
             printf("\nFinished reading. Please update the progress before close the book");
             printf("\nEnter the page you are currently on: ");
             scanf("%d", &page);
-            // if(page > tempBookPtr -> readingProgress -> page) {
-            //     struct ReadingProgress* pushProgress = (struct ReadingProgress*) malloc(sizeof(struct ReadingProgress));
-            //     pushProgress -> page = page;
-            //     pushProgress -> nextPtr = NULL;
-            //     tempBookPtr -> readingProgress -> nextPtr = pushProgress;
-            // } else if (page < tempBookPtr -> readingProgress -> page) {
-                
-            // }
+            if(page > tempBookPtr -> readingProgress -> page) {
+                struct ReadingProgress* pushProgress = (struct ReadingProgress*) malloc(sizeof(struct ReadingProgress));
+                pushProgress -> page = page;
+                pushProgress -> nextPtr = tempBookPtr -> readingProgress;
+                tempBookPtr -> readingProgress = pushProgress;
+            } else if (page < tempBookPtr -> readingProgress -> page) {
+                while (page <= tempBookPtr -> readingProgress -> page) {
+                    struct ReadingProgress* deleteProgress = tempBookPtr -> readingProgress;
+                    tempBookPtr -> readingProgress = tempBookPtr -> readingProgress -> nextPtr;
+                    free(deleteProgress);
+                }
+                struct ReadingProgress* pushProgress = (struct ReadingProgress*) malloc(sizeof(struct ReadingProgress));
+                pushProgress -> page = page;
+                pushProgress -> nextPtr = tempBookPtr -> readingProgress;
+                tempBookPtr -> readingProgress = pushProgress;
+            }
         }
     }
 }
@@ -340,10 +346,10 @@ void deleteBook(struct Book** headBook) {
 	int position;
 	printf("\nEnter the book index you want to delete[Starting from 1]: ");
 	scanf("%d", &position);
-	
+
 	struct Book* deletePtr = *headBook;
 	struct Book* tempPtr;
-	
+
 	if(position == 1) {
         *headBook = (*headBook)->nextPtr;
     } else {
@@ -362,7 +368,7 @@ void deleteBook(struct Book** headBook) {
 
 
 /*int login(struct user u1, struct user u2, struct user u3, struct user u4) {
-    
+
     char loginUsername[30], loginPassword[30];
     int loginAttempts = 3, switchLoginSignUp;
 
